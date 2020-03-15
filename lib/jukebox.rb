643b7d0ec0741886c392e21d3f32b4c4f8e6afd8
Enrique Pittaluga <ep7m@me.com>
2020-03-15 18:38:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -49,6 +49,18 @@ def run()
 
 should_run = true
 
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
 
   while (should_run) do 
     puts "Please enter a command:"
